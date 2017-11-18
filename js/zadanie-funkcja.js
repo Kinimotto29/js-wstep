4d7a7b8b6d9254a063093c7bceabc9405b831456
Kinimotto29 <dommin5@interia.pl>
2017-11-18 16:53:57 +0100

    progres

---
+++ js/zadanie-funkcja.js
@@ -0,0 +1,9 @@
+function iloczyn(parametr1, parametr2, parametr3) {
+    var result;
+    
+    result = parametr1 * parametr2 * parametr3;
+    
+    return result;
+}
+var global = iloczyn(3,4,3);
+console.log(global);
